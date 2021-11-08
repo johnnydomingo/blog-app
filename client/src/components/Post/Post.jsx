e0a94b11ff6685fdc6d759b1952174fd657711c3
Mike <mnezz1131@gmail.com>
2021-11-08 15:40:40 -0600

    Post screen/Post Component

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -5,3 +5,19 @@ Author
 Content
 
 */
+import { Link } from "react-router-dom";
+import "./Post.css"
+
+const Post = (props) => {
+  return(
+    <div>
+      <Link className ="post" to={`/posts/${props._id}`}>
+        <div className="post-title">{props.title}</div>
+        <div className="post-author">{props.author}</div>
+        <div className="post-content">{props.content}</div>
+      </Link>
+    </div>
+  )
+};
+
+export default Post
